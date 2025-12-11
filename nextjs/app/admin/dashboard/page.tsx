5c61a1ac5a6cd5f520a71bad7418eed9ec2fabca
Muhammadshajid <141731578+Muhammadshajid@users.noreply.github.com>
2025-12-11 21:57:52 +0400

    Delete nextjs directory

--- nextjs/app/admin/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-'use client'
-import Link from 'next/link'
-export default function Dashboard(){ return (<div className="p-8"><h2 className="text-2xl">Admin Dashboard</h2><div className="mt-4 space-x-3"><Link href='/admin/blogs'>Blogs</Link> <Link href='/admin/software'>Software</Link> <Link href='/admin/experts'>Experts</Link></div></div>)}
--- nextjs/app/blog/[slug]/page.tsx
+++
@@ -1,6 +0,0 @@
-export default async function BlogPost({ params }: { params: { slug: string }}){
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/blog-get?slug=${params.slug}`);
-  const json = await res.json();
-  const blog = json.blog || { title: 'Not found', content: '' };
-  return (<div className="container mx-auto p-8"><h1>{blog.title}</h1><div dangerouslySetInnerHTML={{ __html: blog.content || '' }} /></div>);
-}
