a66e38b2c3d5bbdfe31cb29c9b30f3b622711798
Muhammadshajid <141731578+Muhammadshajid@users.noreply.github.com>
2025-12-18 15:08:50 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function HomePage() {
             <Link href="/services" className="btn-primary">
               Explore Services
             </Link>
-            <Link href="/contact" className="btn btn-outline-dark btn-lg">
+            <button href="/contact" className="btn bg-white text-dark border-dark">
               Talk to an Expert
             </Link>
           </div>
