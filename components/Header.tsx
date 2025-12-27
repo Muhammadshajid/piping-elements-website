8cdfe1d1ffcc16056f6bf606685dfd8edd48b58d
Muhammadshajid <141731578+Muhammadshajid@users.noreply.github.com>
2025-12-27 13:56:45 +0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -104,12 +104,12 @@ export default function Header() {
             >
               Request Consultation
             </Link>
-            <Link
+            {/* <Link
               href="/"
               className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors"
             >
               Request Software Demo
-            </Link>
+            </Link> */}
           </div>
 
           {/* Mobile menu button */}
