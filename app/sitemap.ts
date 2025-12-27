9a85a377e4de3bae66837697b340259018dd66df
Muhammadshajid <141731578+Muhammadshajid@users.noreply.github.com>
2025-12-27 15:27:12 +0400

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,9 @@
 export default function sitemap() {
   return [
-    { url: "https://yourdomain.com", lastModified: new Date() },
-    { url: "https://yourdomain.com/about", lastModified: new Date() },
-    { url: "https://yourdomain.com/services", lastModified: new Date() },
-    { url: "https://yourdomain.com/blog", lastModified: new Date() },
-    { url: "https://yourdomain.com/contact", lastModified: new Date() },
+    { url: "https://pipingelements.com", lastModified: new Date() },
+    { url: "https://pipingelements/about", lastModified: new Date() },
+    { url: "https://pipingelements/services", lastModified: new Date() },
+    { url: "https://pipingelements/blog", lastModified: new Date() },
+    { url: "https://pipingelements/contact", lastModified: new Date() },
   ];
 }
