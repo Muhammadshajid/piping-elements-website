1aa319404f3a003977c05b741a11bbfaa498372a
Muhammadshajid <141731578+Muhammadshajid@users.noreply.github.com>
2026-01-10 09:09:12 +0400

    Update sitemap URLs to include 'www'

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,9 @@
 export default function sitemap() {
   return [
     { url: "https://pipingelements.com", lastModified: new Date() },
-    { url: "https://pipingelements/about", lastModified: new Date() },
-    { url: "https://pipingelements/services", lastModified: new Date() },
-    { url: "https://pipingelements/blog", lastModified: new Date() },
-    { url: "https://pipingelements/contact", lastModified: new Date() },
+    { url: "https://pipingelements.com/about", lastModified: new Date() },
+    { url: "https://pipingelements.com/services", lastModified: new Date() },
+    { url: "https://pipingelements.com/blog", lastModified: new Date() },
+    { url: "https://pipingelements.com/contact", lastModified: new Date() },
   ];
 }
